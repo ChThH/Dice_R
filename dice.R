99261452148a8a7372abf7ce9879f0433c5ccc26
CT Hall <cthall83@gmail.com>
2021-06-16 15:12:47 -0700

    Expanded Dice.R

---
+++ dice.R
@@ -0,0 +1,22 @@
+### Dice info ###
+
+dice <- function(num, sides){
+  sample(sides, num, TRUE)
+}
+
+dice_reroll <- function(num, sides, reroll, times=0){
+  rolls <- dice(num, sides)
+  if ((length(sides) == 1 && all(1:sides %in% reroll)) ||
+      (all(sides %in% reroll))){
+    print('Reroll values cannot be identical to all sides.')
+  } else if (times == 0){
+    while (any(rolls %in% reroll)){
+      rolls[rolls %in% reroll] <- dice(length(rolls[rolls %in% reroll]),sides)
+    } 
+  } else {
+    for(time in times){
+      rolls[rolls %in% reroll] <- dice(length(rolls[rolls %in% reroll]),sides)
+    }
+  }
+    return(rolls)
+}
